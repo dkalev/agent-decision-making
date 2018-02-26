67cc2d11eb8e65db0416e4ddbb1125cfa86e6058
Damyan Kalev <dzkalev@gmail.com>
2018-02-26 19:43:21 +0800

    Defined state and maze layout

---
+++ src/GridMaze.java
@@ -0,0 +1,64 @@
+
+public class GridMaze {
+	private static final int NUM_ROW = 6;
+	private static final int NUM_COL = 6;
+	
+	private static final double WHITE_REWARD = -0.04;
+	private static final double GREEN_REWARD = +1.0;
+	private static final double ORANGE_REWARD = -1.0;
+	
+	private State[][] grid;
+	
+	public GridMaze() {
+		grid = new State[NUM_ROW][NUM_COL];
+		buildOriginalMaze();
+	}
+	
+	private void buildOriginalMaze() {
+		grid[0][0] = new State(GREEN_REWARD);
+		grid[0][1] = new State(State.WALL);
+		grid[0][2] = new State(GREEN_REWARD);
+		grid[0][3] = new State(WHITE_REWARD);
+		grid[0][4] = new State(WHITE_REWARD);
+		grid[0][5] = new State(GREEN_REWARD);
+		
+		grid[1][0] = new State(WHITE_REWARD);
+		grid[1][1] = new State(ORANGE_REWARD);
+		grid[1][2] = new State(WHITE_REWARD);
+		grid[1][3] = new State(GREEN_REWARD);
+		grid[1][4] = new State(State.WALL);
+		grid[1][5] = new State(ORANGE_REWARD);
+		
+		grid[2][0] = new State(WHITE_REWARD);
+		grid[2][1] = new State(WHITE_REWARD);
+		grid[2][2] = new State(ORANGE_REWARD);
+		grid[2][3] = new State(WHITE_REWARD);
+		grid[2][4] = new State(GREEN_REWARD);
+		grid[2][5] = new State(WHITE_REWARD);
+		
+		grid[3][0] = new State(WHITE_REWARD);
+		grid[3][1] = new State(WHITE_REWARD);
+		grid[3][2] = new State(WHITE_REWARD);
+		grid[3][3] = new State(ORANGE_REWARD);
+		grid[3][4] = new State(WHITE_REWARD);
+		grid[3][5] = new State(GREEN_REWARD);
+		
+		grid[4][0] = new State(WHITE_REWARD);
+		grid[4][1] = new State(State.WALL);
+		grid[4][2] = new State(State.WALL);
+		grid[4][3] = new State(State.WALL);
+		grid[4][4] = new State(ORANGE_REWARD);
+		grid[4][5] = new State(WHITE_REWARD);
+		
+		grid[5][0] = new State(WHITE_REWARD);
+		grid[5][1] = new State(WHITE_REWARD);
+		grid[5][2] = new State(WHITE_REWARD);
+		grid[5][3] = new State(WHITE_REWARD);
+		grid[5][4] = new State(WHITE_REWARD);
+		grid[5][5] = new State(WHITE_REWARD);
+	}
+	
+	public State[][] getGridMaze(){
+		return grid;
+	}
+}
---
+++ src/State.java
@@ -0,0 +1,25 @@
+
+public class State {
+	public static final boolean WALL = true;
+	
+	private double reward;
+	private boolean isWall;
+		
+	public State(double reward) {
+		this.reward = reward;
+		isWall = false;
+	}
+	
+	public State(boolean isWall) {
+		this.isWall = isWall;
+		this.reward = 0.0;
+	}
+	
+	public double getReward() {
+		return reward;
+	}
+	
+	public boolean isWall() {
+		return isWall;
+	}
+}
